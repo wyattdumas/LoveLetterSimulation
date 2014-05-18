346d9cb4b9ea8fa392d846d407145bbf799da992
jonathanp <jonathanp@GENSUP-002.trd.state.nm.us>
2014-05-18 14:16:33 -0600

    Starting Classes for Deck and Driver

--- LoveLetterSimulation/love/Deck.java
+++ LoveLetterSimulation/love/Deck.java
@@ -1,13 +1,41 @@
 package love;
+import java.util.Vector;
 
 public class Deck {
 
-	private int numCards = 16;
-	private Card Available[];
-	private Card Discard;
+	private int activeCards;
+	private int discardCards;
+	private Vector<Card> available;
+	private Vector<Card> discard;
 	
-	public Deck()
+	public Deck(int numPlayers)
+	{
+		if(numPlayers == 2) {
+			activeCards = 12;
+			discardCards = 4;
+		} else if(numPlayers > 2) {
+			activeCards = 15;
+			discardCards = 1;
+		}
+		
+		//available = new Vector()
+		//discard = new Card[discardCards];
+		
+	}
+	
+	private void Shuffle()
 	{
 		
+	}
+	
+	public Card DrawTopCard()
+	{
+		Card topCard = available.remove(0);
+		return topCard;
+	}
+	
+	public boolean hasCards()
+	{
+		return !available.isEmpty();
 	}
 }
