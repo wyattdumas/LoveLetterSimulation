acd48c8d82d6269bf08c8da8836d5b4e26a9894b
wyattdumas <wyatt.dumas@gmail.com>
2014-05-18 14:16:17 -0600

    skeleton Player class with constructor, play(), and draw(Deck drawPile)

--- LoveLetterSimulation/love/Player.java
+++ LoveLetterSimulation/love/Player.java
@@ -1,5 +1,22 @@
 package love;
+import java.util.*;
 
 public class Player {
 
+	private Vector<Card> hand = new Vector<Card>();
+	private Vector<Card> discardPile = new Vector<Card>(); 
+	private int position = 0;
+	
+	public Player (){
+		
+	}
+	
+	public void draw(Deck drawPile){
+		//hand.add(drawPile.drawTopCard());		
+	}
+	
+	public void play(){		
+		System.out.println(hand.remove(0));
+	}	
+	
 }
